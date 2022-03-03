70350c1e7ecedf64b24d85c2fa8ec9ffd020fdf9
Caspar Velsen-Zerweck <caspar@velsen-zerweck.de>
2022-03-03 14:59:08 +0100

    Undo

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <h1 className="headline">Shopping list</h1>
+      <p className="header__subtitle">Structure your next Shopping</p>
+    </header>
+  );
+}
---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <>
+      <h2 class>Your shopping list</h2>
+    </>
+  );
+}
